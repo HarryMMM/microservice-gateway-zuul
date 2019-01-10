1709dff8ae5ae2f38e0bb3ea0be462a0f953d830
harry <hhr_harry@qq.com>
2019-01-10 23:12:12 +0800

    修改自定义过滤器实例方式

--- src/main/java/cn/harryai/microservicegatewayzuul/filters/PreRequestLogFilter.java
+++ src/main/java/cn/harryai/microservicegatewayzuul/filters/PreRequestLogFilter.java
@@ -6,11 +6,15 @@ import com.netflix.zuul.exception.ZuulException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 
+
+@Component
 public class PreRequestLogFilter extends ZuulFilter {
-    private static final Logger LOGGER=LoggerFactory.getLogger(PreRequestLogFilter.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(PreRequestLogFilter.class);
+
     @Override
     public String filterType() {
         return FilterConstants.PRE_TYPE;
@@ -18,7 +22,7 @@ public class PreRequestLogFilter extends ZuulFilter {
 
     @Override
     public int filterOrder() {
-        return FilterConstants.PRE_DECORATION_FILTER_ORDER-1;
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER - 1;
     }
 
     @Override
@@ -28,9 +32,9 @@ public class PreRequestLogFilter extends ZuulFilter {
 
     @Override
     public Object run() throws ZuulException {
-        RequestContext requestContext=RequestContext.getCurrentContext();
+        RequestContext requestContext = RequestContext.getCurrentContext();
         HttpServletRequest request = requestContext.getRequest();
-        LOGGER.info("send {} request to {}",request.getMethod(),request.getRequestURL().toString());
+        LOGGER.info("send {} request to {}", request.getMethod(), request.getRequestURL().toString());
 
 
         return null;
